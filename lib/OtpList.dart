143f6e9348764e51847f995b0a72f805e703b173
Marc-Antoine <madube94@gmail.com>
2018-12-21 00:23:53 -0500

    feat(otp): add timer to refresh otp code

--- lib/OtpList.dart
+++ lib/OtpList.dart
@@ -4,15 +4,32 @@ import 'package:otp/otp.dart';
 
 class OtpList extends StatefulWidget {
   final List<OtpItem> otpItems = [
-    OtpItem('asdf', 'Github', 'dubemarcantoine'),
+    OtpItem('prem7biaeqvisstc', 'Github', 'dubemarcantoine'),
     OtpItem('asdfasdf', 'Github', 'dubemarcantoine'),
   ];
 
+  OtpList() {
+    this.generateCodes();
+  }
+
   @override
   _OtpListState createState() => _OtpListState();
+
+  void generateCodes() {
+    this.otpItems.forEach((otpItem) => otpItem.otpCode = OTP.generateTOTPCode(
+        otpItem.secret, DateTime.now().millisecondsSinceEpoch));
+  }
 }
 
 class _OtpListState extends State<OtpList> {
+  @override
+  void initState() {
+    final int timeUntilNextRefresh = (DateTime.now().second - 30).abs();
+    print(timeUntilNextRefresh);
+    Future.delayed(
+        Duration(seconds: timeUntilNextRefresh), () => _startTimer());
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
@@ -24,14 +41,25 @@ class _OtpListState extends State<OtpList> {
           return Column(
             children: <Widget>[
               ListTile(
-                title: Text(
-                    "${OTP.generateTOTPCode(item.secret, DateTime.now().millisecondsSinceEpoch)}"),
+                title: Text("${item.otpCode}"),
                 subtitle: Text(item.issuer),
               ),
               Divider(),
             ],
           );
         });
+  }
+
+  void _setOtpCodesFromSecrets() {
+    setState(() {
+      widget.generateCodes();
+    });
+  }
+
+  void _startTimer() {
+    this._setOtpCodesFromSecrets();
+    new Timer.periodic(
+        Duration(seconds: 30), (Timer t) => this._setOtpCodesFromSecrets());
   }
 }
 
