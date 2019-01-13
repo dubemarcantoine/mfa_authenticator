e3324dcf2069e04fc35d991e3fa168596a47f18c
Marc-Antoine <madube94@gmail.com>
2019-01-13 11:48:34 -0500

    refactor(home): remove useless homepage component

--- lib/main.dart
+++ lib/main.dart
@@ -20,23 +20,7 @@ class App extends StatelessWidget {
         primaryColorDark: Colors.red,
         toggleableActiveColor: Colors.red,
       ),
-      home: HomePage(title: 'Authenticator'),
+      home: OtpList(title: 'Authenticator'),
     );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  HomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return OtpList(widget.title);
   }
 }
