913a29b72081e8ec0f4495c389f3f424bd705ac3
Marc-Antoine Dube <madube94@gmail.com>
2019-01-16 00:06:50 -0500

    fix(auth): start local authentication

--- lib/SecurityConfig.dart
+++ lib/SecurityConfig.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:local_auth/local_auth.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SecurityConfig extends StatefulWidget {
+  static const String IS_USING_AUTH_KEY = 'IS_USING_AUTH';
+
   @override
   _SecurityConfigState createState() => _SecurityConfigState();
 }
 
 class _SecurityConfigState extends State<SecurityConfig> {
-  final String IS_USING_AUTH_KEY = 'IS_USING_AUTH';
-
+  final LocalAuthentication _localAuthentication = LocalAuthentication();
+  bool _canCheckBiometrics;
+  List<BiometricType> _availableBiometrics;
   SharedPreferences _preferences;
   bool _isUsingAuth = false;
+  Function _func = null;
 
   void initState() {
     _initPreferences();
@@ -26,21 +31,58 @@ class _SecurityConfigState extends State<SecurityConfig> {
         child: SwitchListTile(
           value: _isUsingAuth,
           title: Text('Enable authentication'),
-          onChanged: (bool newValue) {
-            setState(() {
-              _isUsingAuth = newValue;
-              _preferences.setBool(IS_USING_AUTH_KEY, _isUsingAuth);
-            });
-          },
+          onChanged: _func,
         ),
       ),
     );
   }
 
   void _initPreferences() async {
-    _preferences = await SharedPreferences.getInstance();
+    await _checkBiometrics();
+    if (_canCheckBiometrics) {
+      await _getAvailableBiometrics();
+    }
+    if (_availableBiometrics.length > 0) {
+      _func = (bool newValue) {
+        setState(() {
+          _isUsingAuth = newValue;
+          _preferences.setBool(
+              SecurityConfig.IS_USING_AUTH_KEY, _isUsingAuth);
+        });
+      };
+      _preferences = await SharedPreferences.getInstance();
+      setState(() {
+        _isUsingAuth =
+            _preferences.getBool(SecurityConfig.IS_USING_AUTH_KEY) ?? false;
+      });
+    }
+  }
+
+  Future<void> _getAvailableBiometrics() async {
+    List<BiometricType> availableBiometrics;
+    try {
+      availableBiometrics = await _localAuthentication.getAvailableBiometrics();
+    } on Exception catch (e) {
+      print(e);
+      return;
+    }
+
     setState(() {
-      _isUsingAuth = _preferences.getBool(IS_USING_AUTH_KEY);
+      _availableBiometrics = availableBiometrics;
+    });
+  }
+
+  Future<void> _checkBiometrics() async {
+    bool canCheckBiometrics;
+    try {
+      canCheckBiometrics = await _localAuthentication.canCheckBiometrics;
+    } on Exception catch (e) {
+      print(e);
+    }
+    if (!mounted) return;
+
+    setState(() {
+      _canCheckBiometrics = canCheckBiometrics;
     });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,45 @@
-import 'dart:math';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:mfa_authenticator/ManualEntry.dart';
+import 'package:local_auth/local_auth.dart';
 import 'package:mfa_authenticator/OtpList.dart';
-import 'package:mfa_authenticator/ScanCodeEntry.dart';
+import 'package:mfa_authenticator/SecurityConfig.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() => runApp(App());
+void main() => runApp(Base());
 
-class App extends StatelessWidget {
+class Base extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return App();
+  }
+}
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  SharedPreferences _preferences;
+
+  var _result = null;
+
+  @override
+  void initState() {
+    authenticate().then((res) {
+      setState(() {
+        _result = res;
+      });
+    });
+  }
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    if (_result == null || !_result) {
+      return Container();
+    }
     return MaterialApp(
       title: 'Authenticator',
       theme: ThemeData(
@@ -22,5 +51,25 @@ class App extends StatelessWidget {
       ),
       home: OtpList(title: 'Authenticator'),
     );
+  }
+
+  Future<bool> authenticate() async {
+    _preferences = await SharedPreferences.getInstance();
+    bool result = _preferences.getBool(SecurityConfig.IS_USING_AUTH_KEY);
+    if (!result) {
+      return Future.value(true);
+    } else {
+      var localAuth = LocalAuthentication();
+      List<BiometricType> availableBiometrics =
+          await localAuth.getAvailableBiometrics();
+      if (Platform.isIOS) {
+        if (availableBiometrics.contains(BiometricType.face)) {
+          return await localAuth.authenticateWithBiometrics(
+              localizedReason: 'Please authenticate to view your codes');
+        } else if (availableBiometrics.contains(BiometricType.fingerprint)) {
+          // Touch ID.
+        }
+      }
+    }
   }
 }
