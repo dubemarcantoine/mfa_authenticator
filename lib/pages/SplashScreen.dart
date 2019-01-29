ba60a60c3c464e871b4e64f8429b4aa17680652b
Marc-Antoine Dube <madube94@gmail.com>
2019-01-28 20:24:52 -0500

    feat(splashscreen): modify app splashscreen

--- lib/pages/SplashScreen.dart
+++ lib/pages/SplashScreen.dart
@@ -4,7 +4,21 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Container(
-      decoration: new BoxDecoration(color: Color.fromRGBO(46, 46, 46, 1.0)),
+      decoration: new BoxDecoration(
+        color: Color.fromRGBO(46, 46, 46, 1.0),
+      ),
+      child: Center(
+        child: new Container(
+          width: 256.0,
+          height: 256.0,
+          decoration: new BoxDecoration(
+            image: new DecorationImage(
+              image: new AssetImage('assets/logo.png'),
+              repeat: ImageRepeat.repeat,
+            ),
+          ),
+        ),
+      )
     );
   }
 }
